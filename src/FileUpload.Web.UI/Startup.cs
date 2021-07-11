fd499925bcfeaf94ff9648cb7f673896a9f3c148
so <John_ming@outlook.com>
2021-07-11 12:54:48 +0800

    1

--- src/FileUpload.Web.UI/Startup.cs
+++ src/FileUpload.Web.UI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 
 namespace FileUpload
@@ -25,8 +26,7 @@ namespace FileUpload
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddControllersWithViews();
 
             services
                 .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
@@ -78,15 +78,26 @@ namespace FileUpload
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.EnvironmentName.Equals("Development", StringComparison.OrdinalIgnoreCase))
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
+            }
             else
+            {
                 app.UseExceptionHandler("/error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
 
             app.UseAuthentication();
             app.UseStatusCodePages();
-            app.UseStaticFiles();
-            app.UseMvc();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
